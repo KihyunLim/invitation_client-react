7259a6c6055ed68033f6960747a813e652ba0f6a
lkh_corp <a47165705@gmail.com>
2020-09-11 17:34:04 +0900

    청첩장 프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Loading from "./components/Loading";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    invitation: undefined,
+  };
+
+  getInvitationData = async () => {
+    let data = undefined;
+
+    await axios
+      .get(
+        "http://localhost:8980/admin/invitation/receiveInvitation.do?invSeq=6"
+      )
+      .then((res) => {
+        if (res.data.resFlag) {
+          data = res.data;
+        } else {
+          // 에러 컴포넌트로 대체 해야하나??
+        }
+      })
+      .catch((res) => {
+        // 에러 컴포넌트로 대체 해야하나??
+        console.log("fail");
+        console.log(res);
+      });
+
+    console.log(data);
+  };
+
+  componentDidMount() {
+    this.getInvitationData();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    return isLoading ? <Loading /> : <div>isLoading is false</div>;
+  }
 }
 
 export default App;
---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <section id="loading">
+      <div>loading...</div>
+    </section>
+  );
+}
+
+export default Loading;
