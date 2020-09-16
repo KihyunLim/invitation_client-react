620d8add8e55a09b78c12a38ac73cc92c99e5ea2
KHLIMNOTEBOOK\khlim\eng\sourcetree <a47165705@gmail.com>
2020-09-17 00:44:46 +0900

    지도 api 연동 어렵다아 아직 진행중..

--- src/components/WhenWhere.js
+++ src/components/WhenWhere.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import * as Secret from '../common/secret.js';
+import axios from 'axios';
 
 function WhenWhere({ whenWhere }) {
   return (
-    <section id="whenWhere">
-      <h1 className="whenWhere__title">When &amp; Where</h1>
-      <div className="whenWhere__ceremony">
+    <section id='whenWhere'>
+      <h1 className='whenWhere__title'>When &amp; Where</h1>
+      <div className='whenWhere__ceremony'>
         {whenWhere.map((itemWhenWhere, index) => {
           return (
             <ItemWhenWhere key={index} seq={index} whenWhere={itemWhenWhere} />
@@ -21,21 +22,21 @@ function ItemWhenWhere({ seq, whenWhere }) {
   const { background, icon, type } = setInfo(seq);
 
   return (
-    <div className="ceremony__item" style={background}>
-      <div className="wrap__item">
-        <p className="item__decoration">
+    <div className='ceremony__item' style={background}>
+      <div className='wrap__item'>
+        <p className='item__decoration'>
           <i className={icon}></i>
         </p>
-        <h2 className="item__title">{type}</h2>
-        <p className="item__date">{getFormatDate(dateWedding)}</p>
-        <p className="item__time">{getFromatTime(timeWedding)}</p>
-        <p className="item__place">{address}</p>
+        <h2 className='item__title'>{type}</h2>
+        <p className='item__date'>{getFormatDate(dateWedding)}</p>
+        <p className='item__time'>{getFromatTime(timeWedding)}</p>
+        <p className='item__place'>{address}</p>
         <img
-          className="item__map"
+          className='item__map'
           src={getImageMap(placeX, placeY)}
-          alt="map image"
+          alt='map image'
         />
-        <button className="item__move_web">지도 보기</button>
+        <button className='item__move_web'>지도 보기</button>
       </div>
     </div>
   );
@@ -77,18 +78,61 @@ function getFromatTime(time) {
   return `${flagAmPm} ${hour} : ${time.substr(2, 2)}`;
 }
 
-function getImageMap(placeX, placeY) {
-  return (
-    'https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?' +
-    'w=300&h=300' +
-    '&center=' +
-    String(placeX) +
-    ',' +
-    String(placeY) +
-    '&level=16' +
-    '&X-NCP-APIGW-API-KEY-ID=' +
-    Secret.naver_map_key
-  );
+async function getImageMap(placeX, placeY) {
+  let src = 'test';
+  // let xhr = new XMLHttpRequest();
+  // xhr.responseType = 'blob';
+  // xhr.onreadystatechange = function () {
+  //   console.log('!!!!!!!!!!!!!!!!!!!');
+  //   if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+  //     console.log('@@@@@@@@@@@@@@');
+  //     src = URL.createObjectURL(xhr.response);
+  //   }
+  // };
+  // xhr.open(
+  //   'GET',
+  //   'https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?' +
+  //     'w=300&h=300' +
+  //     '&center=' +
+  //     String(placeX) +
+  //     ',' +
+  //     String(placeY) +
+  //     '&level=16' +
+  //     '&X-NCP-APIGW-API-KEY-ID=' +
+  //     Secret.NAVER_MAP_CLIENT_ID,
+  //   true
+  // );
+  // xhr.setRequestHeader('X-NCP-APIGW-API-KEY', Secret.NAVER_MAP_CLIENT_SECRET);
+  // xhr.send();
+
+  // https://github.com/axios/axios https://qastack.kr/programming/45578844/how-to-set-header-and-options-in-axios
+  await axios
+    .get({
+      url:
+        'https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?' +
+        'w=300&h=300' +
+        '&center=' +
+        String(placeX) +
+        ',' +
+        String(placeY) +
+        '&level=16' +
+        '&X-NCP-APIGW-API-KEY-ID=' +
+        Secret.NAVER_MAP_CLIENT_ID,
+      method: 'get',
+      headers: { 'X-NCP-APIGW-API-KEY': Secret.NAVER_MAP_CLIENT_SECRET },
+      responseType: 'blob',
+    })
+    .then((res) => {
+      console.log('success');
+      console.log(res);
+    })
+    .catch((res) => {
+      console.log('fail');
+      console.log(res);
+    });
+
+  console.log(src);
+  return src;
 }
 
 export default WhenWhere;
