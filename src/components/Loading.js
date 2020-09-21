838925c565b7b3cdeaf44a3d5f6a6516545e53ca
lkh_corp <a47165705@gmail.com>
2020-09-21 17:53:20 +0900

    로딩 중/실패 화면 css 수정, 모바일 css 추가 (모바일일 때 글자 크기 적용안되는거 같음)

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-function Loading() {
+const Loading = () => {
+  const [dot, setDot] = useState('');
+
+  const effectLoading = () => {
+    setDot((prevDot) => {
+      return prevDot.length >= 5 ? '' : prevDot + '.';
+    });
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(effectLoading, 500);
+
+    return () => {
+      console.log('clear!!');
+      clearInterval(timerID);
+    };
+  }, []);
+
   return (
     <section id="loading">
-      <div>loading...</div>
+      <div className="loading__text">
+        <span>loading</span>
+        <span>{dot}</span>
+      </div>
     </section>
   );
-}
+};
 
 export default Loading;
--- src/components/LoadingFail.js
+++ src/components/LoadingFail.js
@@ -3,7 +3,9 @@ import React from 'react';
 function LoadingFail() {
   return (
     <section id="loading_fail">
-      <div>loading fail!!</div>
+      <div className="loading__text">
+        청접장 정보를 불러오는데 실패했습니다ㅠㅠ
+      </div>
     </section>
   );
 }
